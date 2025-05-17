4137177eed7e6e65cc09b108c86dd20741900a03
alkasadist <alkasadist@gmail.com>
2025-05-17 21:06:10 +0300

    INIT COMMIT

---
+++ store-server/store/products/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, HttpResponse
+
+from products.models import ProductCategory, Product
+# Create your views here.
+# функции = контроллеры
+
+
+def index(request):
+    context = {
+        'title': 'Store',
+    }
+
+    return render(request, 'products\index.html', context)
+
+def products(request):
+    context = {
+        'title': 'Store - Каталог',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
+    }
+    return render(request, 'products\products.html', context)
