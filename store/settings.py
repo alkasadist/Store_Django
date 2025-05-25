db212e946fcb489833249af0a05d6ce046e75497
alkasadist <a1kasadist@ya.ru>
2025-05-25 17:09:44 +0300

    Getting ready for release

--- store/settings.py
+++ store/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oq09$hoqwc+7@596zhgs$d#cw(epvel7q+aa88ha@2g_^e&8m5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
